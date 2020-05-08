3321d59911cb49b09ff31277b687b3262e7c9484
Tommy Philyaw <tommy.philyaw@epandcompany.com>
2020-05-08 11:23:53 -0400

    Serve public

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,5 @@
 import * as express from 'express';
 import * as path from 'path';
-import * as https from 'https';
-import * as fs from 'fs';
 import * as cookieParser from 'cookie-parser';
 import * as config from './config/config';
 import { UnthinkExpressGenerator } from '@epandco/unthink-foundation-express';
@@ -12,6 +10,8 @@ import resourceDefinitions from './resource-definitions';
 const app: express.Application = express();
 app.use(cookieParser());
 
+app.use('/public/', express.static(path.join(process.cwd(), 'public')));
+
 const expressGen = new UnthinkExpressGenerator(app, renderTemplateWithContextAdded, 'debug');
 const unthinkGen = new UnthinkGenerator(expressGen);
 
@@ -20,24 +20,4 @@ resourceDefinitions.forEach(rd => unthinkGen.add(rd));
 unthinkGen.printRouteTable();
 unthinkGen.generate();
 
-// For local development, the webpack dev server is used to serve up bundles
-if (!config.isProduction) {
-  /* eslint-disable */
-  const { forwardToWebpackDevServer } =  require('./webpack-proxy');
-  /* eslint-enable */
-  app.all('/public/js/*', forwardToWebpackDevServer);
-
-  // In production, assets should be served via nginx
-  app.use('/public/', express.static(path.join(process.cwd(), 'public')));
-
-  // Enable HTTPS for local development
-  https.createServer(
-    {
-      key: fs.readFileSync('./certs/localhost.key'),
-      cert: fs.readFileSync('./certs/localhost.crt'),
-    },
-    app
-  ).listen(config.expressServerPort);
-} else {
-  app.listen(config.expressServerPort);
-}
+app.listen(config.expressServerPort);
